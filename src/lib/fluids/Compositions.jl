18094ed44cf770c939451f226649c22de2a598d9
Jerell James <jerell1998@gmail.com>
2023-05-05 12:21:39 +0100

    cleanup

--- src/lib/fluids/Compositions.jl
+++ src/lib/fluids/Compositions.jl
@@ -1,7 +1,7 @@
 module Compositions
 
-compositions = (
-    ; co2impure="REFPROP::CarbonDioxide[0.969696970]&Hydrogen[0.010101010]&Nitrogen[0.010101010]&Argon[0.009090909]&CarbonMonoxide[0.001010101]",
+compositions = (;
+    co2impure="REFPROP::CarbonDioxide[0.969696970]&Hydrogen[0.010101010]&Nitrogen[0.010101010]&Argon[0.009090909]&CarbonMonoxide[0.001010101]",
     co2pure="CarbonDioxide[1.0]"
 )
 
