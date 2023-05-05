3fcb658039bc7a2843e3b91834166edb01b3fc89
Jerell James <jerell1998@gmail.com>
2023-05-05 11:16:22 +0100

    undo lookupfunc

--- src/lib/fluids/Fluids.jl
+++ src/lib/fluids/Fluids.jl
@@ -18,40 +18,28 @@ struct Fluid
     massSpecificEntropy::MassSpecificEntropy
     phase::Float64
 
-    Fluid(lookupfunc::Function, p::Pressure, t::Temperature) = new(
-        p,
-        t,
-        lookupfunc("V", "P", p, "T", t, compositions["100%CO2"]),
-        lookupfunc("D", "P", p, "T", t, compositions["100%CO2"]),
-        lookupfunc("H", "P", p, "T", t, compositions["100%CO2"]),
-        lookupfunc("S", "P", p, "T", t, compositions["100%CO2"]),
-        lookupfunc("Phase", "P", p, "T", t, compositions["100%CO2"]),
-    )
-
-    Fluid(lookupfunc::Function, p::Pressure, h::MassSpecificEnthalpy) = new(
-        p,
-        lookupfunc("T", "P", p, "H", h, compositions["100%CO2"]),
-        lookupfunc("V", "P", p, "H", h, compositions["100%CO2"]),
-        lookupfunc("D", "P", p, "H", h, compositions["100%CO2"]),
-        h,
-        lookupfunc("S", "P", p, "H", h, compositions["100%CO2"]),
-        lookupfunc("Phase", "P", p, "H", h, compositions["100%CO2"]),
-    )
-
     # The equations of state are based on T and ρ as state variables,
     # so T,ρ will always be the fastest inputs. P,T will be a bit slower (3-10 times), 
     # followed by input pairs where neither T nor ρ are specified, like P,H; these will be much slower. 
     # If speed is an issue, you can look into table-based interpolation methods using TTSE or bicubic interpolation;
     Fluid(p::Pressure, t::Temperature) = new(
-        PropsSI,
         p,
-        t
+        t,
+        PropsSI("V", "P", p, "T", t, compositions["100%CO2"]),
+        PropsSI("D", "P", p, "T", t, compositions["100%CO2"]),
+        PropsSI("H", "P", p, "T", t, compositions["100%CO2"]),
+        PropsSI("S", "P", p, "T", t, compositions["100%CO2"]),
+        PropsSI("Phase", "P", p, "T", t, compositions["100%CO2"]),
     )
 
     Fluid(p::Pressure, h::MassSpecificEnthalpy) = new(
-        PropsSI,
         p,
-        h
+        PropsSI("T", "P", p, "H", h, compositions["100%CO2"]),
+        PropsSI("V", "P", p, "H", h, compositions["100%CO2"]),
+        PropsSI("D", "P", p, "H", h, compositions["100%CO2"]),
+        PropsSI("H", "P", p, "H", h, compositions["100%CO2"]),
+        PropsSI("S", "P", p, "H", h, compositions["100%CO2"]),
+        PropsSI("Phase", "P", p, "H", h, compositions["100%CO2"]),
     )
 end
 
